bd00ba6ff2378f470430bccd9ae77cb0e49e8cac
helioncode <elioncode99@gmail.com>
2023-06-11 13:49:24 +0700

    add math function

--- Basic/Mathf.cs
+++ Basic/Mathf.cs
@@ -2,5 +2,41 @@ using System;
 
 public class Math
 {
-    
+    static void Main(string[] args){
+        #region Math function
+        double number = -5.5;
+
+        // Absolute value
+        double absoluteValue = Math.Abs(number);
+        Console.WriteLine("Absolute value of " + number + " is: " + absoluteValue);
+
+        // Square root
+        double squareRoot = Math.Sqrt(25);
+        Console.WriteLine("Square root of 25 is: " + squareRoot);
+
+        // Power
+        double power = Math.Pow(2, 3);
+        Console.WriteLine("2 raised to the power of 3 is: " + power);
+
+        // Floor
+        double floorValue = Math.Floor(3.8);
+        Console.WriteLine("Floor value of 3.8 is: " + floorValue);
+
+        // Ceiling
+        double ceilingValue = Math.Ceiling(3.2);
+        Console.WriteLine("Ceiling value of 3.2 is: " + ceilingValue);
+
+        // Round
+        double roundedValue = Math.Round(4.6);
+        Console.WriteLine("Rounded value of 4.6 is: " + roundedValue);
+
+        // Mathematical constant pi
+        double pi = Math.PI;
+        Console.WriteLine("The value of pi is: " + pi);
+
+        // Mathematical constant e
+        double e = Math.E;
+        Console.WriteLine("The value of e is: " + e);
+        #endregion
+    }
 }
